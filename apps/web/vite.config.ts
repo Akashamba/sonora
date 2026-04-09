3d38faf681cbd9b2bdc675015f64e5f3afccb976
akashamba <akashambashankar@gmail.com>
2026-04-10 02:25:14 +0530

    vite app able to talk to server in dev using proxy to avoid cors issues

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/home": "http://localhost:3000",
+      "/release-group": "http://localhost:3000",
+    },
+  },
   plugins: [react()],
-})
+});
